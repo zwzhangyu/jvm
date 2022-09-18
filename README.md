78e413a5a2e2c1a2784fb91527d5c17277d783c8
zhangyu <zhangyu_techlife@163.com>
2022-09-18 22:12:43 +0800

    虚拟机栈

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jvm
 jvm学习笔记
+
+## chapter2 类加载子系统
+
---
+++ chapter4/src/com/zy/demo1/Main.java
@@ -0,0 +1,23 @@
+package com.zy.demo1;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        String str = "Hello";
+    }
+
+
+    // 测试slot
+    public String test1(int a1, char a2) {
+        long a3 = 100L;
+        double a4 = 100.0;
+        String str = "hello";
+        int a5 = 1000;
+        return str;
+    }
+
+
+}
