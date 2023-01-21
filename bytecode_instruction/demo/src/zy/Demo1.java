d7fec8a7f79d6cbfbc5af5345c2e21ed69159c31
zhangyu <zhangyu_techlife@163>
2023-01-21 21:49:16 +0800

    JVM指令

---
+++ bytecode_instruction/demo/src/zy/Demo1.java
@@ -0,0 +1,18 @@
+package zy;
+
+/**
+ * 测试
+ *
+ * @author zhangyu
+ * @date 2023/1/20
+ */
+public class Demo1 {
+
+    private String name;
+
+    private int i;
+
+    public String hello(){
+        return name;
+    }
+}
---
+++ bytecode_instruction/demo/src/zy/LoadAndStoreTest.java
@@ -0,0 +1,26 @@
+package zy;
+
+/**
+ * 测试加载与存储指令
+ *
+ * @author zhangyu
+ * @date 2023/1/20
+ */
+public class LoadAndStoreTest {
+
+    /**
+     * 局部变量压栈指令
+     */
+    public void load(int num, Object obj, long count, boolean flag, short[] arr) {
+        System.out.println(num);
+        System.out.println(obj);
+        System.out.println(count);
+        System.out.println(flag);
+        System.out.println(arr);
+    }
+
+
+
+
+
+}
